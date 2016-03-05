b1ed0a8ed71148d632eac561eb18aab27b2e7921
Peroumalle <mperouma@gmail.com>
2016-03-05 11:42:08 +0100

    update to api 14

--- app/src/main/java/fr/upem/trashmapupem/LoginActivity.java
+++ app/src/main/java/fr/upem/trashmapupem/LoginActivity.java
@@ -171,6 +171,7 @@ public class LoginActivity extends AppCompatActivity{
                 String[] pieces = credential.split(":");
                 if (pieces[0].equals(mEmail)) {
                     // Account exists, return true if the password matches.
+
                     return pieces[1].equals(mPassword);
                 }
             }
