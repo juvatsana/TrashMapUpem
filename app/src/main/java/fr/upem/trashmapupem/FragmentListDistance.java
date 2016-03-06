ed82a3b121469b4dfb3671225161a8d4a9ba1d64
Peroumalle <mperouma@gmail.com>
2016-03-06 21:40:28 +0100

    Ajout de list distance  avec recyclerview et cardview parce que c'est plus propre

--- app/src/main/java/fr/upem/trashmapupem/FragmentListDistance.java
+++ app/src/main/java/fr/upem/trashmapupem/FragmentListDistance.java
@@ -3,6 +3,9 @@ package fr.upem.trashmapupem;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,33 +26,64 @@ import java.util.List;
  */
 public class FragmentListDistance extends Fragment {
 
-    public static Fragment newInstance(Context context) {
-        FragmentListDistance f = new FragmentListDistance();
-        return f;
+
+    class Data {
+        String type;
+        String distance;
+        int photoPb;
+
+        Data(String type, String distance, int photoPb) {
+            this.type = type;
+            this.distance = distance;
+            this.photoPb = photoPb;
+        }
     }
+
+    private List<Data> datas = new ArrayList<>();
+    private RecyclerView rv;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_listdistance, container, false);
-        ListView listview = (ListView) root.findViewById(R.id.listDistanceView);
-        final List<LatLng> thelist = FragmentMap.getPosOfMapMark();
-        List<String> listCoord = calculListPos(thelist);
-        final StableArrayAdapter adapter = new StableArrayAdapter(getContext(),android.R.layout.simple_list_item_1, listCoord);
-        listview.setAdapter(adapter);
+//        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_listdistance, container, false);
+//        ListView listview = (ListView) root.findViewById(R.id.listDistanceView);
+//        final List<LatLng> thelist = FragmentMap.getPosOfMapMark();
+//        List<String> listCoord = calculListPos(thelist);
+//        final StableArrayAdapter adapter = new StableArrayAdapter(getContext(),android.R.layout.simple_list_item_1, listCoord);
+//        listview.setAdapter(adapter);
 
+        Log.i("OnCreateView","before everything");
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_recyclerview_distance, container, false);
+        rv= (RecyclerView)root.findViewById(R.id.recycle_pb);
+        rv.setHasFixedSize(true);
+        Log.i("OnCreateView", "before after set has fixed size");
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(layoutManager);
+        rv.setItemAnimator(new DefaultItemAnimator());
+
+        final List<LatLng> thelist = FragmentMap.getPosOfMapMark();
+        calculListPos(thelist);
+        Log.i("OnCreateView","before adapter");
+        RVAdapter adapter = new RVAdapter(datas);
+        rv.setAdapter(adapter);
         return root;
     }
 
-    public List<String> calculListPos(List<LatLng> listlat)
+    public void calculListPos(List<LatLng> listlat)
     {
-        List<String> listdouble = new ArrayList<>();
+
         for(LatLng ll:listlat)
         {
             Double distance = CalculationByDistance(new LatLng(48.838790, 2.585753), ll);
-            listdouble.add(String.valueOf(distance)+" km");
+            Data d = new Data("green",String.valueOf(distance)+" km",R.drawable.pb);
+            datas.add(d);
         }
-        return listdouble;
+
+    }
+
+    public static Fragment newInstance(Context context) {
+        FragmentListDistance f = new FragmentListDistance();
+        return f;
     }
 
     public double CalculationByDistance(LatLng StartP, LatLng EndP) {
