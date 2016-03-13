425d9cc2a14268beb8433bb9a0e01a81f96d54ec
juvatsana <julienvorabout@hotmail.fr>
2016-03-13 22:01:43 +0100

    Add comment PoubelleMarker

--- app/src/main/java/fr/upem/trashmapupem/PoubelleMarker.java
+++ app/src/main/java/fr/upem/trashmapupem/PoubelleMarker.java
@@ -3,23 +3,36 @@ package fr.upem.trashmapupem;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
- * Created by Juan on 08/03/2016.
+ * Class qui permet de représenter le marquer d'une poubelle
  */
 public class PoubelleMarker {
 
     private FragmentMap.FM_TYPE type;
     private MarkerOptions marker;
 
+    /**
+     * Creer une nouvelle instance de marker de poubelle
+     * @param type Correspond au type de poubelle
+     * @param marker Contient toutes les informations du marker (longitude,latitude...)
+     */
     public PoubelleMarker (FragmentMap.FM_TYPE type,MarkerOptions marker)
     {
         this.type = type;
         this.marker = marker;
     }
 
+    /**
+     * Enum qui correspond au type de poubelle
+     * @return le type de poubelle
+     */
     public FragmentMap.FM_TYPE getType() {
         return type;
     }
 
+    /**
+     * Informations du marker
+     * @return les informations relatives au marker
+     */
     public MarkerOptions getMarkerOptions()
     {
         return marker;
